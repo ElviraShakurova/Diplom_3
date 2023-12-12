3fe7786c2f441e85373e6655953c4a56dfba886c
Elvira Shakurova <elenshakurova@gmail.com>
2023-12-12 22:32:29 +0300

    add solution classes

---
+++ src/test/java/tests/MainTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import io.qameta.allure.Description;
+import org.junit.Rule;
+import org.junit.Test;
+import pages.MainPage;
+
+public class MainTest {
+    @Rule
+    public DriverRule driverRule = new DriverRule();
+
+    @Description("Тест проводит проверку перехода к разделам Булки, Соусы, Начинки")
+    @Test
+    public void testIngredientTabs(){
+        MainPage basePage = new MainPage(driverRule.getDriver())
+                .clickSouses()
+                .assertSousesIngredient()
+                .clickFillings()
+                .assertFillingIngredient()
+                .clickBuns()
+                .assertBunsIngredient();
+    }
+}
